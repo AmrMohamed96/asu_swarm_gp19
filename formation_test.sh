1e000cea8247fbf0cc3316fbc9b95a8a73131f8e
Amr Mohamed <amr.moh@hotmail.com>
2019-07-03 22:25:17 +0200

    New build files. New launch file

--- formation_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gnome-terminal --tab -e "bash -c \"rosrun swarm_formation_19 global_knowledge19.py; exec bash\"" --tab -e "bash -c \"rosrun swarm_formation_19 hp_range_detector.py; exec bash\"" --tab -e "bash -c \"rosrun swarm_formation_19 formation1.py; exec bash\"" --tab -e "bash -c \"rosrun swarm_formation_19 formation2.py; exec bash\"" --tab -e "bash -c \"rosrun swarm_formation_19 formation3.py; exec bash\"" --tab -e "bash -c \"rosrun swarm_formation_19 formation4.py; exec bash\""
-
-$SHELL
